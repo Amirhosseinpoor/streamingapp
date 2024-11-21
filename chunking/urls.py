a06d30ce312ac1502758fe23bb4eee0a45e1b861
Amirhosseinpoor <amirhosseinpoor@example.com>
2024-11-21 23:50:22 +0330

    add flutter project to main

--- chunking/urls.py
+++ chunking/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import VideoListView, UploadView
+from django.views.generic import TemplateView
 urlpatterns = [
     path('show/', VideoListView.as_view(), name='video_list'),
-    path('', UploadView.as_view(), name='upload')
+    path('', UploadView.as_view(), name='upload'),
+    path('pwa', TemplateView.as_view(template_name='pwa/index.html'), name='pwa'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chunking.urls'))
+    path('', include('chunking.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
