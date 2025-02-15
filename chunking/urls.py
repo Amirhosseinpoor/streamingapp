c63d5677f25380042a1efafc8c783e8c1f8a1cc1
Amirhosseinpoor <amirhosseinpoor@example.com>
2025-02-15 16:09:50 +0330

    nothing

--- chunking/urls.py
+++ chunking/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('show/', VideoListView.as_view(), name='video_list'),
     path('', UploadView.as_view(), name='upload'),
     path('pwa', TemplateView.as_view(template_name='pwa/index.html'), name='pwa'),
+
 ]
